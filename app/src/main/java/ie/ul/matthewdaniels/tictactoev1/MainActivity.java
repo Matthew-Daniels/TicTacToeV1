35abeba7f208fca9a9b3906bcdac940e322edfd0
Matthew-Daniels <31405959+Matthew-Daniels@users.noreply.github.com>
2018-10-11 12:24:22 +0100

    Finished View

--- app/src/main/java/ie/ul/matthewdaniels/tictactoev1/MainActivity.java
+++ app/src/main/java/ie/ul/matthewdaniels/tictactoev1/MainActivity.java
@@ -2,6 +2,8 @@ package ie.ul.matthewdaniels.tictactoev1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGame = new TicTacToeGame(this);
+    }
+
+    public void pressedSquare(View view) {
+        String tagAsStr = view.getTag().toString();
+        int tagAsInt = Integer.parseInt(tagAsStr);
+        //Log.d("TTT","You pressed index" + tagAsInt);
+
+        // Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void pressedNewGame(View view) {
+        Toast.makeText(this, " New Game", Toast.LENGTH_SHORT).show();
+
     }
 }
